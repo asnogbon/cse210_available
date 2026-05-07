ce76a3e0324e1ce1c8632714a535a73e4ebf1a93
asnogbon <ayomideashogbon123@gmail.com>
2026-05-07 13:06:09 -0700

    Refactor Program to include user interaction for name and favorite number

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        
+        string userName = "y";
+        int userNumber = 0;
+
+        DisplayMessage();
+        DisplayPersonalMessage(userName);
+        DisplayPersonalNumber(userNumber);
+
+
+        static void DisplayMessage()
+        {
+            Console.WriteLine("Welcome to the Program!");
+        }
+
+        static string DisplayPersonalMessage(string userName)
+        {
+            Console.Write($"Please enter your name: ");
+            userName = Console.ReadLine();
+            return userName;
+        }
+
+        static int DisplayPersonalNumber(int userNumber)
+        {
+            Console.WriteLine($"Please enter your favorite number: {userNumber}");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int square = number * number;
+            return square;
+        }
+
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
     }
+    
 }
