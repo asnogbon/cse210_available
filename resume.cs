e3dfab3c3acb006a44832f3bbf42823007f5f3c5
asnogbon <ayomideashogbon123@gmail.com>
2026-05-13 00:37:14 -0700

    Fix Journal classes; update exercises 3-5

---
+++ resume.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class resume
+{
+    public string _name;
+    {
+    }
+    public public List<job> _jobs = new Listjob>(); _jobs()
+    {
+    }
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        // Notice the use of the custom data type "Job" in this loop
+        foreach (Job job in _jobs)
+        {
+            // This calls the Display method on each job
+            job.Display();
+        }
+    }
