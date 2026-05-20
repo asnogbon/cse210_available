3fcddbe2e5b0f825411ec8107a60adb89807a042
asnogbon <ayomideashogbon123@gmail.com>
2026-05-20 11:03:16 -0700

    run
    Add three console exercises: name, grading, guessing game
    
    Implemented three exercises:
    1. Name prompt and formatted output.
    2. Grade percentage to letter grade with pass/fail.
    3. Number guessing game with replay option.

---
+++ Referance.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+
+public class Scripture
+{
+    public Referance _referance()
+    {
+    }
+    public List<Word> _word()
+    {
+    }
+    public Scripture(Referance Referance, string text)
+    {
+    }
+    public void HideRadomWords(int numberToHide)
+    {
+    }
+    public string GetDisplayText()
+    {
+    }
+    public bool IsCompleteHidden()
+    {
+    }
+}
