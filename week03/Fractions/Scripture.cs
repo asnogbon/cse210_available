3d3b19aa37db59927dedf59ed9620385061ea946
asnogbon <ayomideashogbon123@gmail.com>
2026-05-20 10:33:22 -0700

    Add initial implementation of Scripture class with reference and word methods

---
+++ week03/Fractions/Scripture.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+
+public class Scripture
+{
+    public Referance _referance()
+    {
+    }
+    public List<Word> _word()
+    {
+    }
+    public Referance _referance()
+    {
+    }
+}
