912361cb56c1a4991368e8102f1f165696c27bd7
asnogbon <ayomideashogbon123@gmail.com>
2026-05-28 16:44:23 -0700

    Add Video and Comment classes; implement video listing and comment display functionality

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class Comment
+{
+    private string Name;
+    private string Text;
+    int count = 0;
+
+    public string GetComment(string commented)
+    {
+        string[] Parts = commented.Split(':');
+        count ++;
+        Name = Parts[0];
+        Text = Parts[1];
+
+        return $"User {Name} : said \"{Text}\"";
+    }
+    public string GetCount(string commented)
+    {
+        return $"This video has {count} comments.";
+    }
+
+}
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,45 @@
+﻿using System;
+public class Video
+{
+    private string Title;
+    private string Author;
+    private int Length;
+    public string GetVideo(string title)
+    {
+        string[] titleParts = title.Split(',');
+        Title = titleParts[0];
+        Author = titleParts[1];
+        string tt = getLength(titleParts[2]);
+        Length = int.Parse(tt);
+        return $"{Title},{Author},{Length}";
+    }
+
+    public string getLength(string time)
+    {
+        string[] timeParts = time.Split(':');
+        if (timeParts.Length == 2)
+        {
+            int hours = int.Parse(timeParts[0]);
+            int minutes = int.Parse(timeParts[1]);
+            int seconds = int.Parse(timeParts[2]);
+            int totalSeconds = hours * 3600 + minutes * 60 + seconds;
+            return totalSeconds.ToString(); 
+        }
+        
+        else if (timeParts.Length == 1)
+        {
+            int minutes = int.Parse(timeParts[0]);
+            int seconds = int.Parse(timeParts[1]);
+            int totalSeconds = minutes * 60 + seconds;
+            return totalSeconds.ToString();
+        }
+
+        else if (timeParts.Length == 0)
+        {
+            int seconds = int.Parse(timeParts[0]);
+            return seconds.ToString();
+        }
+
+    return "0";
+    }
+}
